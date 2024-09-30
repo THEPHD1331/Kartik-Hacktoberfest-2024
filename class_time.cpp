b4ca0ec84aeaccfd626883ca416d22770bb4544e
Kartik jain <148695895+Perfect-Piixel@users.noreply.github.com>
2024-09-30 20:46:24 +0530

    Add files via upload

---
+++ class_time.cpp
@@ -0,0 +1,26 @@
+<<<<<<< HEAD
+#include<iostream>
+
+class Time{
+    private:
+    int hour;
+    int minute;
+    int second;
+
+    void 
+
+
+=======
+#include<iostream>
+
+class Time{
+    private:
+    int hour;
+    int minute;
+    int second;
+
+    void 
+
+
+>>>>>>> 1b7018d398ee3c878bbc71fbcac1e3bd854c7f73
+}
